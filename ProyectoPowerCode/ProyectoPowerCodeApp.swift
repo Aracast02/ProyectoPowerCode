1b3055497dbc3bdc57f94a51231595c5c3e4c436
Aracast02 <ary.castro.dessavre@gmail.com>
2023-03-29 13:35:42 -0600

    Initial Commit

---
+++ ProyectoPowerCode/ProyectoPowerCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProyectoPowerCodeApp.swift
+//  ProyectoPowerCode
+//
+//  Created by Arantza Castro on 29/03/23.
+//
+
+import SwiftUI
+
+@main
+struct ProyectoPowerCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
